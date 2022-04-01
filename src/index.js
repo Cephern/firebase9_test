40fa12e31ee47bd0af9551d32f702e5d2a7a444e
JagMax <gewoonmaks@gmail.com>
2022-04-01 16:17:19 +0300

    React@18 Firebase@9

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { AuthContextProvider } from './context/AuthContext';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AuthContextProvider><App /></AuthContextProvider>);
